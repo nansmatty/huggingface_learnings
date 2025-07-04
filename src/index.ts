677abe8e69adfa873d5e32608740854919e76315
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-07-04 17:19:30 +0530

    text to image

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { InferenceClient } from '@huggingface/inference';
+import { writeFileSync } from 'fs';
 
 const inference = new InferenceClient(process.env.HF_TOKEN_FINE_GRAINED);
 
@@ -31,6 +32,23 @@ async function answerQuestion() {
 	console.log(result);
 }
 
+async function textToImage() {
+	const result = await inference.textToImage(
+		{
+			inputs: 'Dog in the hat on a mat.',
+			model: 'black-forest-labs/FLUX.1-dev',
+			parameters: {
+				negative_prompt: 'blurry and unclear',
+			},
+		},
+		{ outputType: 'blob' }
+	);
+
+	writeFileSync('image.png', Buffer.from(await result.arrayBuffer()));
+	console.log('Image saved successfully');
+}
+
 // embed()
 // translate();
-answerQuestion();
+// answerQuestion();
+textToImage();
